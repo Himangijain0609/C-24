8dcc2673690394ded84d52b5a1298ce0f051e77e
Himangijain0609 <73227035+Himangijain0609@users.noreply.github.com>
2020-12-11 15:27:10 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,28 @@
+class Pig{
+    constructor(x,y){
+        var option = {
+            restitution:1.0,
+            friction:1.0,
+            density:1.0 
+        }
+        this.width=50;
+        this.height=50;
+this.body = Bodies.rectangle(x,y,50,50,option);
+World.add(world,this.body);
+    }
+display(){
+    var pos=this.body.position
+    //pos.x=mouseX;
+   // pos.y=mouseY;
+    var angle=this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER)
+    stroke("white");
+    fill("pink")
+    rect(0,0,this.width,this.height);
+    pop();
+}
+    
+}
